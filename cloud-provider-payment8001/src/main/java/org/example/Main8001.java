cc82731b2a84938e0c9d17277066bd7a3be59070
fuxuebin <256527739@qq.com>
2024-09-21 15:27:34 +0800

    Initial commit

---
+++ cloud-provider-payment8001/src/main/java/org/example/Main8001.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan("org.example.mapper") //import tk.mybatis.spring.annotation.MapperScan;
+public class Main8001
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Main8001.class,args);
+    }
+}
